a8060c5f929462f3098eb8ae8144c8a71c85363b
Dani Pando Moreno <34524302+RedWolf3121@users.noreply.github.com>
2019-01-02 12:49:09 +0100

    NOTAS

--- app/src/main/java/com/example/gymtoday1/CrearNotaActivity.java
+++ app/src/main/java/com/example/gymtoday1/CrearNotaActivity.java
@@ -1,12 +1,9 @@
 package com.example.gymtoday1;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.widget.CardView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,17 +14,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class CrearNotaActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_crear_nota);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CrearNotaActivity.this, NotasActivity.class);
+                startActivity(intent);
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -37,40 +41,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        CardView favoritos = (CardView) findViewById(R.id.cardFavourite);
-        favoritos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FavouritesActivity.class);
-                startActivity(intent);
-            }
-        });
-        CardView ejercicios = (CardView) findViewById(R.id.cardEjercicios);
-        ejercicios.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EjerciciosActivity.class);
-                startActivity(intent);
-            }
-        });
-        CardView dietas = (CardView) findViewById(R.id.cardDietas);
-        dietas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DietasActivity.class);
-                startActivity(intent);
-            }
-        });
-        CardView rutina = (CardView) findViewById(R.id.cardRutina);
-        rutina.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RutinaActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 
     @Override
@@ -86,7 +56,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.crear_nota, menu);
         return true;
     }
 
@@ -112,36 +82,20 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent,0);
-
-            // Sólo se lanzará el activity si hay apps disponibles en el sistema para ejecutarla.
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            }
-
+            // Handle the camera action
         } else if (id == R.id.nav_crono) {
-            Intent intent = new Intent(MainActivity.this, CronometroActivity.class);
-            startActivity(intent);
 
         } else if (id == R.id.nav_Calendario) {
-            Intent intent = new Intent(MainActivity.this, CalendarioActivity.class);
-            startActivity(intent);
 
         } else if (id == R.id.nav_Notas) {
-            Intent intent = new Intent(MainActivity.this, NotasActivity.class);
-            startActivity(intent);
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_TEXT, "GymToday");
-            startActivity(Intent.createChooser(intent, "Share with"));
-        }
 
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
